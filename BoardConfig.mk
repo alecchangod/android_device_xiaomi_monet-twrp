083dcfbe2555fabe1cf1f6102d3a55bdda648f94
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-04-12 17:02:52 +0800

    build kernel image

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,15 +19,13 @@
 # product configuration (apps).
 #
 
-# Prebuilts
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-
 # Inherit from OEM SoC-common
 -include $(COMMON_PATH)/BoardConfigCommon.mk
 
 # Prebuilts
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/$(BOARD_KERNEL_IMAGE_NAME)
+# TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
+# BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+# TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/$(BOARD_KERNEL_IMAGE_NAME)
 
 # Recovery
 TARGET_RECOVERY_DEVICE_DIRS += $(DEVICE_PATH)
